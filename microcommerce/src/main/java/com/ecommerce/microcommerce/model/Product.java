855788f7e39d445dfad10ccad158366ef0fbcdd8
Martho Evan <evanom59@gmail.com>
2018-12-11 20:13:20 +0100

    :construction: ajout de la class permettant les exceptions

--- microcommerce/src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -2,10 +2,12 @@ package com.ecommerce.microcommerce.model;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 
 
 @Entity
@@ -16,7 +18,10 @@ public class Product {
     private int id;
 
 
+    @Length(min = 3, max = 20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
     private String nom;
+
+    @Min(value = 1)
     private int prix;
 
     private int prixAchat;
--- microcommerce/src/main/java/com/ecommerce/microcommerce/web/controller/ProduitIntrouvableException.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/web/controller/ProduitIntrouvableException.java
@@ -1,8 +1,13 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProduitIntrouvableException extends RuntimeException {
 
     public ProduitIntrouvableException(String s) {
         super(s);
     }
 }
+
